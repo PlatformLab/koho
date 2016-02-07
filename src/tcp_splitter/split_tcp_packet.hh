547b6c5283b2a2b72f8eb9c5d80dba0f15885f58
Greg D. Hill <greghill@cs.stanford.edu>
2016-02-06 19:21:51 -0800

    stringstreams are dumb, checking in and doing something else

--- src/tcp_splitter/split_tcp_packet.hh
+++ src/tcp_splitter/split_tcp_packet.hh
@@ -5,10 +5,11 @@
 
 #include <string>
 #include <sstream>
+#include <iostream>
 
 struct SplitTCPPacket {
-    enum connection_type { NEW, EXISTING } conn_type;
-    uint64_t uid = 1;
+    enum connection_type : uint8_t { NEW, EXISTING } conn_type;
+    uint64_t uid;
     union {
         struct {
             std::string address;
@@ -24,27 +25,52 @@ struct SplitTCPPacket {
         switch (conn_type) {
             case NEW:
                 new_connection.address.~basic_string<char>();
+                break;
             case EXISTING:
                 existing_connection.body.~basic_string<char>();
+                break;
         }
     };
 
 
     std::string toString() const {
-        std::stringstream ret;
-        ret << uid << conn_type;
+        std::cerr << "serialization attempt" << std::endl;
+        std::ostringstream ret;
+        ret << (uint8_t) conn_type;
+        ret << uid ;
+        std::cerr << "serialization going" << std::endl;
         switch (conn_type) {
             case NEW:
-                ret << new_connection.address << new_connection.port;
+                std::cerr << "serialization new" << std::endl;
+                ret << new_connection.address;
+                std::cerr << "serialization new address done" << std::endl;
+                ret << new_connection.port;
+                break;
             case EXISTING:
-                ret << existing_connection.eof << existing_connection.body;
+                std::cerr << "serialization existing" << std::endl;
+                ret << existing_connection.eof;
+                std::cerr << "serialization existing eof done" << std::endl;
+                ret << existing_connection.body;
+                break;
         }
+
+        std::cerr << "good one serialized is " << ret.str() << std::endl;
+        SplitTCPPacket other( ret.str() );
+        std::cerr << "made other " << std::endl;
+        assert( conn_type == other.conn_type );
+        std::cerr << "uid " << uid  << " while other " << other.uid << std::endl;
+        assert( uid == other.uid );
         switch (conn_type) {
             case NEW:
-                ret << new_connection.address << new_connection.port;
+                assert( new_connection.address == other.new_connection.address );
+                assert( new_connection.port == other.new_connection.port );
+                break;
             case EXISTING:
-                ret << existing_connection.eof << existing_connection.body;
+                assert( existing_connection.eof == other.existing_connection.eof );
+                assert( existing_connection.body == other.existing_connection.body );
+                break;
         }
+        std::cerr << "serialization good" << std::endl;
         return ret.str();
     };
 
@@ -64,15 +90,22 @@ struct SplitTCPPacket {
 
     /* Parse from string */
     SplitTCPPacket(std::string serialized) 
-    : conn_type(*reinterpret_cast<const enum connection_type *>( serialized.data() ))
+    : conn_type( NEW ), uid(-1)
     { 
-        std::stringstream str( serialized.data() + sizeof(enum connection_type) );
+        std::istringstream str( serialized.data() );
+        uint8_t temp_type;
+        str >> temp_type;
+        conn_type = (connection_type) temp_type;
+        uid = 0;
         str >> uid;
+        std::cerr << "got uid " << uid << std::endl;
         switch (conn_type) {
             case NEW:
                 str >> new_connection.address >> new_connection.port;
+                break;
             case EXISTING:
                 str >> existing_connection.eof >> existing_connection.body;
+                break;
         }
     };
 };
