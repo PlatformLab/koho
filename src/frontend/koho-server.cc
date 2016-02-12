abf38269636101874a733de4cafa6be455f212aa
Greg D. Hill <greghill@cs.stanford.edu>
2016-02-11 20:50:11 -0800

    run dns proxy server on koho-server

--- src/frontend/koho-server.cc
+++ src/frontend/koho-server.cc
@@ -8,6 +8,8 @@
 #include "address.hh"
 #include "interfaces.hh"
 #include "ezio.hh"
+#include "dns_proxy.hh"
+#include "event_loop.hh"
 #include "tcp_splitter_server.hh"
 
 
@@ -17,9 +19,22 @@ int main( int argc, char *argv[] )
         throw std::runtime_error( "Usage: " + std::string( argv[ 0 ] ) );
     }
 
-    const Address splitter_server_listen;
+    const Address nameserver = first_nameserver();
+    const Address splitter_server_listen, dns_server;
 
+    EventLoop main_event_loop;
+    DNSProxy dns_outside( dns_server, nameserver, nameserver );
     TCP_Splitter_Server tcp_splitter_server = TCP_Splitter_Server( splitter_server_listen );
+
     std::cerr << "koho-client 127.1 " << tcp_splitter_server.local_address().port() << std::endl;
-    return tcp_splitter_server.loop();
+
+    main_event_loop.add_child_process( "tcp-splitter-server", [&]() {
+            drop_privileges(); // TODO needed?
+
+            return tcp_splitter_server.loop();
+            } );
+
+    dns_outside.register_handlers( main_event_loop );
+
+    return main_event_loop.loop();
 }
