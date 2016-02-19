277c94fad6d7f9bc54003d0f5ecfa2ffd999d7e7
Greg D. Hill <greghill@cs.stanford.edu>
2016-02-18 22:19:43 -0800

    Clean up epoller.

--- src/util/epoller.cc
+++ src/util/epoller.cc
@@ -12,30 +12,23 @@ Epoller::Epoller( )
     actions_()
 {
     epoll_fd_ = SystemCall( "epoll_create", ::epoll_create( 1 ) );
-    if ( epoll_fd_ < 0 ) {
-        throw runtime_error( "Couldn't create epoller" );
-    }
 }
 
 void Epoller::add_action( Epoller::Action action )
 {
     actions_.emplace( action.fd.fd_num(), action );
 
-    struct epoll_event ev;
+    epoll_event ev;
     ev.events = action.direction == Direction::In ? EPOLLIN : EPOLLOUT;
     ev.data.fd = action.fd.fd_num();
 
-    if ( SystemCall( "epoll_ctl", ::epoll_ctl( epoll_fd_, EPOLL_CTL_ADD, action.fd.fd_num(), &ev ) ) != 0 )
-    {
-        cerr << "epoll insertion error on fd " << action.fd.fd_num() << endl;
-    }
+    SystemCall( "epoll_ctl", ::epoll_ctl( epoll_fd_, EPOLL_CTL_ADD, action.fd.fd_num(), &ev ) );
 }
 
 void Epoller::remove_action( int file_descriptor )
 {
-    if ( SystemCall( "epoll_ctl", ::epoll_ctl( epoll_fd_, EPOLL_CTL_DEL, file_descriptor, NULL ) ) != 0 ) { // "Applications that need to be portable to kernels before 2.6.9 should specify a non-null pointer in event"
-        cerr << "epoll deletion error on fd " << file_descriptor << endl;
-    }
+    // Note: "Applications that need to be portable to kernels before 2.6.9 should specify a non-null pointer in event"
+    SystemCall( "epoll_ctl", ::epoll_ctl( epoll_fd_, EPOLL_CTL_DEL, file_descriptor, NULL ) );
 
     size_t num_erased = actions_.erase( file_descriptor );
     if ( num_erased != 1 ) {
@@ -56,11 +49,6 @@ Epoller::Result Epoller::poll( const int & timeout_ms )
 
     int num_fds = SystemCall( "epoll_ctl", ::epoll_wait( epoll_fd_, events, max_events, timeout_ms ) );
    
-    if ( num_fds < 0 ) {
-        cerr << "epoll poll error" << endl;
-        return Result::Type::Exit;
-    }
-
     for (int n = 0; n < num_fds; n++) {
         auto action_it = actions_.find( events[n].data.fd );
         assert( action_it != actions_.end() );
